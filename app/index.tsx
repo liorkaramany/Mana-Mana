60f2a6f3fca9eb2915b18eaa1241996a40d52c4f
Lior Karamany <lior.karamany@gmail.com>
2024-06-11 10:47:33 +0300

    create home screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,15 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { useFonts } from "expo-font";
 import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
 import "react-native-reanimated";
-import { AppText } from "./components/AppText";
+import { HomeScreen } from "./screens/HomeScreen";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   const [loaded] = useFonts({
@@ -22,5 +26,12 @@ export default function App() {
     return null;
   }
 
-  return <AppText>Config</AppText>;
+  return (
+    <Stack.Navigator
+      screenOptions={{ title: "Mana Mana" }}
+      initialRouteName="Home"
+    >
+      <Stack.Screen name="Home" component={HomeScreen} />
+    </Stack.Navigator>
+  );
 }
---
+++ app/screens/HomeScreen/HomeScreen.tsx
@@ -0,0 +1,10 @@
+import { AppText } from "@/app/components/AppText";
+import { View } from "react-native";
+
+export const HomeScreen = () => {
+  return (
+    <View>
+      <AppText>Home Screen</AppText>
+    </View>
+  );
+};
