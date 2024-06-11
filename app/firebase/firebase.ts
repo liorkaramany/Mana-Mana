9501f7992da7b4e5380df604862a36c8ac4df49c
Lior Karamany <lior.karamany@gmail.com>
2024-06-11 15:40:00 +0300

    fix firebase initialization issue

--- app/firebase/firebase.ts
+++ app/firebase/firebase.ts
@@ -1,12 +1,13 @@
-import { initializeApp } from "firebase/app";
+import { getApp, getApps, initializeApp } from "firebase/app";
 
 export const firebaseConfig = {
-  apiKey: process.env.EXPO_FIREBASE_API_KEY,
-  authDomain: process.env.EXPO_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.EXPO_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.EXPO_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.EXPO_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.EXPO_FIREBASE_APP_ID,
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
 };
 
-export const app = initializeApp(firebaseConfig);
+export const app =
+  getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
--- app/screens/LoginScreen/LoginScreen.tsx
+++ app/screens/LoginScreen/LoginScreen.tsx
@@ -1,10 +1,17 @@
 import { AppText } from "@/app/components/AppText";
 import { Colors } from "@/app/config/Colors";
+import { app } from "@/app/firebase";
 import { StackParamList } from "@/app/types/navigation";
+import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import { getReactNativePersistence, initializeAuth } from "firebase/auth";
 import { Button, View } from "react-native";
 
 export type LoginScreenProps = NativeStackScreenProps<StackParamList, "Login">;
+
+const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(ReactNativeAsyncStorage),
+});
 
 export const LoginScreen = (props: LoginScreenProps) => {
   const { navigation } = props;
