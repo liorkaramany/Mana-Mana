68a3d2f343ae3eefe8eff3aaf85efe890deb03fc
Lior Karamany <lior.karamany@gmail.com>
2024-06-14 14:50:28 +0300

    add user type and create relation between user and recipe

--- app/models/feed.ts
+++ app/models/feed.ts
@@ -1,9 +1,12 @@
+import { User } from "./user";
+
 export type RecipeIngredient = {
   name: string;
   amount: number;
 };
 
 export type Recipe = {
+  author: User;
   title: string;
   image: string;
   tags: string[];
