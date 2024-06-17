573047bac5bab1d83550a13d2ccb900017820536
Lior Karamany <lior.karamany@gmail.com>
2024-06-17 19:47:21 +0300

    add ingredients to new recipe

--- app/screens/NewRecipe/NewRecipe.tsx
+++ app/screens/NewRecipe/NewRecipe.tsx
@@ -12,6 +12,7 @@ import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { useEffect, useState } from "react";
 import { ScrollView, View } from "react-native";
 import { styles } from "./styles";
+import { EditableInstructions } from "@/app/components/EditableInstructions";
 
 export type UploadRecipeScreenProps = NativeStackScreenProps<
   StackParamList,
@@ -25,6 +26,7 @@ export const NewRecipe = (props: UploadRecipeScreenProps) => {
   const [selectedCategories, setSelectedCategories] = useState<
     CategoryResponseItem[]
   >([]);
+  const [instructions, setInstructions] = useState<string[]>([""]);
 
   useEffect(() => {
     categoriesApi.getAllCategories().then(setCategories);
@@ -33,19 +35,25 @@ export const NewRecipe = (props: UploadRecipeScreenProps) => {
   return (
     <View style={styles.page}>
       <AppTextInput placeholder="Recipe Title" size="lg" />
-      <ScrollView style={styles.scrollView}>
-        <AppImagePicker />
-        <AppText type="defaultSemiBold">Tags</AppText>
-        <AppMultiSelect
-          items={categories?.categories}
-          uniqueKey="idCategory"
-          displayKey="strCategory"
-          selectedItems={selectedCategories}
-          onSelectedItemsChange={setSelectedCategories}
-        />
-        <AppText type="defaultSemiBold">Ingredients</AppText>
-        <AppText type="defaultSemiBold">Instructions</AppText>
-      </ScrollView>
+      <EditableInstructions
+        instructions={instructions}
+        onInstructionsChange={setInstructions}
+        ListHeaderComponent={
+          <View style={styles.scrollView}>
+            <AppImagePicker />
+            <AppText type="defaultSemiBold">Tags</AppText>
+            <AppMultiSelect
+              items={categories?.categories}
+              uniqueKey="idCategory"
+              displayKey="strCategory"
+              selectedItems={selectedCategories}
+              onSelectedItemsChange={setSelectedCategories}
+            />
+            <AppText type="defaultSemiBold">Ingredients</AppText>
+            <AppText type="defaultSemiBold">Instructions</AppText>
+          </View>
+        }
+      />
     </View>
   );
 };
