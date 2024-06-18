7e7116b49a4e0ff2e824c8054f573a5fadc6c31c
Lior Karamany <lior.karamany@gmail.com>
2024-06-18 21:38:57 +0300

    update recipe form styles

---
+++ app/components/RecipeForm/styles.ts
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  title: {
+    marginBottom: 16,
+  },
+  scrollView: {
+    gap: 8,
+  },
+  uploadButton: {
+    marginTop: 6,
+    marginBottom: 8,
+  },
+});
