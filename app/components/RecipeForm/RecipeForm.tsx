36be727ff2b84f26c52952afa1d5f5b7dd57e3fe
Lior Karamany <lior.karamany@gmail.com>
2024-06-18 21:38:37 +0300

    Create recipe form component

---
+++ app/components/RecipeForm/RecipeForm.tsx
@@ -0,0 +1,83 @@
+import { CategoryResponse, categoriesApi } from "@/app/api/categoriesApi";
+import { AppButton } from "@/app/components/AppButton";
+import { AppImagePicker } from "@/app/components/AppImagePicker";
+import { AppMultiSelect } from "@/app/components/AppMultiSelect";
+import { AppTextInput } from "@/app/components/AppTextInput";
+import { EditableIngredients } from "@/app/components/EditableIngredients";
+import { EditableInstructions } from "@/app/components/EditableInstructions";
+import { NewRecipeSection } from "@/app/components/NewRecipeSection";
+import { DEFAULT_RECIPE_FORM } from "@/app/consts";
+import { Recipe } from "@/app/models/recipe";
+import { useEffect, useState } from "react";
+import { ScrollView, View } from "react-native";
+import { styles } from "./styles";
+
+export type RecipeFormReturnType = Omit<Recipe, "id" | "author">;
+
+export type RecipeFormProps = {
+  categories: CategoryResponse;
+  recipe?: Recipe;
+  onRecipeFormSubmit?: (recipe: RecipeFormReturnType) => void;
+  recipeFormSubmitText?: string;
+};
+
+export const RecipeForm = (props: RecipeFormProps) => {
+  const {
+    categories,
+    recipe,
+    onRecipeFormSubmit,
+    recipeFormSubmitText = "Upload",
+  } = props;
+
+  const [innerRecipe, setInnerRecipe] =
+    useState<RecipeFormReturnType>(DEFAULT_RECIPE_FORM);
+
+  useEffect(() => {
+    if (recipe != null) {
+      setInnerRecipe(recipe);
+    }
+  }, [recipe]);
+
+  const getInnerRecipePropertySetter = <T extends keyof RecipeFormReturnType>(
+    property: T
+  ) => {
+    return (value: RecipeFormReturnType[T]) =>
+      setInnerRecipe({ ...innerRecipe, [property]: value });
+  };
+
+  return (
+    <View>
+      <AppTextInput placeholder="Recipe Title" size="lg" style={styles.title} />
+      <ScrollView contentContainerStyle={styles.scrollView}>
+        <AppImagePicker />
+        <NewRecipeSection title="Tags">
+          <AppMultiSelect
+            items={categories.categories}
+            uniqueKey="idCategory"
+            displayKey="strCategory"
+            selectedItems={innerRecipe.tags}
+            onSelectedItemsChange={getInnerRecipePropertySetter("tags")}
+          />
+        </NewRecipeSection>
+        <NewRecipeSection title="Ingredients">
+          <EditableIngredients
+            ingredients={innerRecipe.ingredients}
+            onIngredientsChange={getInnerRecipePropertySetter("ingredients")}
+          />
+        </NewRecipeSection>
+        <NewRecipeSection title="Instructions">
+          <EditableInstructions
+            instructions={innerRecipe.instructions}
+            onInstructionsChange={getInnerRecipePropertySetter("instructions")}
+          />
+        </NewRecipeSection>
+        <AppButton
+          title={recipeFormSubmitText}
+          onPress={() => onRecipeFormSubmit?.(innerRecipe)}
+          size="lg"
+          style={styles.uploadButton}
+        />
+      </ScrollView>
+    </View>
+  );
+};
