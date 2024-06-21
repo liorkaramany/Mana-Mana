898b8d11cee310e0c7d7fdc3976bfa89e50e8b14
Lior Karamany <lior.karamany@gmail.com>
2024-06-21 11:22:04 +0300

    add loading indicator to app recipe and add messages

--- app/screens/NewRecipe/styles.ts
+++ app/screens/NewRecipe/styles.ts
@@ -6,4 +6,13 @@ export const styles = StyleSheet.create({
     paddingTop: 16,
     paddingHorizontal: 20,
   },
+  loadingOverlay: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 12,
+  },
+  loadingOverlayText: { fontSize: 25 },
+  recipeForm: {
+    flex: 1,
+  },
 });
