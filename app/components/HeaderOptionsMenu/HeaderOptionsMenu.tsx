fc1fbced40020d4bac1f2e664e0f62caca5902b8
Lior Karamany <lior.karamany@gmail.com>
2024-06-25 22:16:41 +0300

    tweak styles and behavior

--- app/components/HeaderOptionsMenu/HeaderOptionsMenu.tsx
+++ app/components/HeaderOptionsMenu/HeaderOptionsMenu.tsx
@@ -3,6 +3,7 @@ import { TouchableHighlight, TouchableOpacity } from "react-native";
 import Popover from "react-native-popover-view";
 import { AppText } from "../AppText";
 import { styles } from "./styles";
+import { useState } from "react";
 
 export type HeaderOptionsMenuProps = {
   onSignOut?: () => void;
@@ -11,17 +12,28 @@ export type HeaderOptionsMenuProps = {
 export const HeaderOptionsMenu = (props: HeaderOptionsMenuProps) => {
   const { onSignOut } = props;
 
+  const [showPopover, setShowPopover] = useState<boolean>(false);
+
   return (
     <Popover
+      popoverStyle={styles.popover}
+      isVisible={showPopover}
+      onRequestClose={() => setShowPopover(false)}
       from={
-        <TouchableOpacity>
+        <TouchableOpacity onPress={() => setShowPopover(true)}>
           <Feather name="more-vertical" size={24} />
         </TouchableOpacity>
       }
     >
-      <TouchableHighlight onPress={onSignOut} style={styles.option}>
+      <TouchableOpacity
+        onPress={() => {
+          setShowPopover(false);
+          onSignOut?.();
+        }}
+        style={styles.option}
+      >
         <AppText>Sign out</AppText>
-      </TouchableHighlight>
+      </TouchableOpacity>
     </Popover>
   );
 };
