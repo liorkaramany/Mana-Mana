59e866f6f888195f81b1cc468d9137c3158e8cc0
Lior Karamany <lior.karamany@gmail.com>
2024-06-26 21:21:00 +0300

    change layout

--- app/components/UserDetailsSection/styles.ts
+++ app/components/UserDetailsSection/styles.ts
@@ -1,12 +1,15 @@
 import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
-  container: {},
+  container: {
+    display: "flex",
+    flexDirection: "row",
+    gap: 20,
+  },
   profileImage: {
     width: 100,
     height: 100,
     borderRadius: 50, // Add rounded corners
-    marginBottom: 10, // Add margin below the image
   },
   name: {},
 });
