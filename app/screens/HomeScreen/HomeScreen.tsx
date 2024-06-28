02d29edceb23a2d15312148be91a87f2b0c78adb
Lior Karamany <lior.karamany@gmail.com>
2024-06-28 10:22:38 +0300

    done in home screen

--- app/screens/HomeScreen/HomeScreen.tsx
+++ app/screens/HomeScreen/HomeScreen.tsx
@@ -1,6 +1,7 @@
 import { AppButton } from "@/app/components/AppButton";
 import { AppText } from "@/app/components/AppText";
 import { AppTextInput } from "@/app/components/AppTextInput";
+import { DeleteModal } from "@/app/components/DeleteModal";
 import { RecipeCard } from "@/app/components/RecipeCard";
 import { Colors } from "@/app/config/Colors";
 import { useAsyncFocused } from "@/app/hooks/useAsyncFocused";
@@ -13,6 +14,8 @@ import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { useState } from "react";
 import { FlatList, TouchableOpacity, View } from "react-native";
 import { styles } from "./styles";
+import Toast from "react-native-toast-message";
+import { AppLoadingOverlay } from "@/app/components/AppLoadingOverlay";
 
 export type HomeScreenProps = NativeStackScreenProps<StackParamList, "Home">;
 
@@ -32,6 +35,9 @@ export const HomeScreen = (props: HomeScreenProps) => {
     action: () => findRecipes(),
     dependencies: [],
   });
+
+  const [deleteModalVisible, setDeleteModalVisible] = useState<boolean>(false);
+  const [deletingRecipe, setDeletingRecipe] = useState<boolean>(false);
 
   if (loading) {
     return (
@@ -62,17 +68,31 @@ export const HomeScreen = (props: HomeScreenProps) => {
   const handleDeletePress = async (recipeId: string) => {
     if (!recipeId) {
       console.error("Missing recipe ID for deletion.");
-      return;
+    } else {
+      try {
+        setDeletingRecipe(true);
+        await deleteRecipe(recipeId);
+        console.log("Recipe deleted successfully!");
+        Toast.show({
+          type: "success",
+          text1: "Success!",
+          text2: "The recipe has been deleted!",
+        });
+        setDeletingRecipe(false);
+
+        await refetchRecipes();
+      } catch (error) {
+        setDeletingRecipe(false);
+        console.error("Error deleting recipe:", error);
+        Toast.show({
+          type: "error",
+          text1: "Oh no!",
+          text2: "There was a problem deleting the recipe, please try again.",
+        });
+      }
     }
 
-    try {
-      await deleteRecipe(recipeId);
-      console.log("Recipe deleted successfully!");
-
-      await refetchRecipes();
-    } catch (error) {
-      console.error("Error deleting recipe:", error);
-    }
+    setDeleteModalVisible(false);
   };
 
   const navigateToRecipe = (recipe: FullRecipe) => {
@@ -88,39 +108,48 @@ export const HomeScreen = (props: HomeScreenProps) => {
   };
 
   return (
-    <View style={styles.container}>
-      <AppTextInput
-        radius="xl"
-        value={search}
-        onChangeText={setSearch}
-        placeholder="Search Recipe"
-        size="lg"
-      />
-      <FlatList
-        refreshing={loading}
-        onRefresh={refetchRecipes}
-        style={styles.recipesList}
-        contentContainerStyle={styles.recipesListItem}
-        data={filteredRecipes}
-        renderItem={({ item: recipe }) => (
-          <TouchableOpacity onPress={() => navigateToRecipe(recipe)}>
-            <RecipeCard
-              recipe={recipe}
-              isInUserFeed={false}
-              onUserPressed={navigateToUser}
-              onDelete={handleDeletePress}
-              onEdit={handleEditPress}
-            />
-          </TouchableOpacity>
-        )}
-        keyExtractor={(item) => item.id}
-      />
-      <AppButton
-        variant="neutral"
-        style={styles.addRecipeButton}
-        title={<Feather name="plus" size={40} color={Colors.tint} />}
-        onPress={() => navigation.navigate("NewRecipe")}
-      />
-    </View>
+    <AppLoadingOverlay loading={deletingRecipe} style={styles.loadingOverlay}>
+      <View style={styles.container}>
+        <AppTextInput
+          radius="xl"
+          value={search}
+          onChangeText={setSearch}
+          placeholder="Search Recipe"
+          size="lg"
+        />
+        <FlatList
+          refreshing={loading}
+          onRefresh={refetchRecipes}
+          style={styles.recipesList}
+          contentContainerStyle={styles.recipesListItem}
+          data={filteredRecipes}
+          renderItem={({ item: recipe }) => (
+            <>
+              <TouchableOpacity onPress={() => navigateToRecipe(recipe)}>
+                <RecipeCard
+                  recipe={recipe}
+                  isInUserFeed={false}
+                  onUserPressed={navigateToUser}
+                  onDelete={() => setDeleteModalVisible(true)}
+                  onEdit={handleEditPress}
+                />
+              </TouchableOpacity>
+              <DeleteModal
+                visible={deleteModalVisible}
+                onClose={() => setDeleteModalVisible(false)}
+                onDelete={() => handleDeletePress(recipe.id)}
+              />
+            </>
+          )}
+          keyExtractor={(item) => item.id}
+        />
+        <AppButton
+          variant="neutral"
+          style={styles.addRecipeButton}
+          title={<Feather name="plus" size={40} color={Colors.tint} />}
+          onPress={() => navigation.navigate("NewRecipe")}
+        />
+      </View>
+    </AppLoadingOverlay>
   );
 };
