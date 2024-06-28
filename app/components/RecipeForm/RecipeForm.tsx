53062ab5afa680fe659b749813c6fcfc6a44d8db
Lior Karamany <lior.karamany@gmail.com>
2024-06-28 17:25:53 +0300

    done with recipe form

--- app/components/RecipeForm/RecipeForm.tsx
+++ app/components/RecipeForm/RecipeForm.tsx
@@ -39,13 +39,15 @@ export const RecipeForm = (props: RecipeFormProps) => {
   const [innerRecipe, setInnerRecipe] =
     useState<RecipeFormValueType>(DEFAULT_RECIPE_FORM);
 
+  const [wasSubmitPressed, setWasSubmitPressed] = useState<boolean>(false);
+
   useEffect(() => {
     if (recipe != null) {
       setInnerRecipe(recipe);
     }
   }, [recipe]);
 
-  const isTitleEmpty = innerRecipe.title.length === 0;
+  const emptyTitle = innerRecipe.title.length === 0;
   const noTags = innerRecipe.tags.length === 0;
   const emptyInstructionsExist = innerRecipe.instructions.some(
     (instruction) => instruction.length === 0
@@ -54,8 +56,8 @@ export const RecipeForm = (props: RecipeFormProps) => {
     (ingredient) => ingredient.name.length === 0 || ingredient.amount === 0
   );
   const noImage = innerRecipe.image == null;
-  const disabledSubmitButton =
-    isTitleEmpty ||
+  const invalidRecipe =
+    emptyTitle ||
     noTags ||
     emptyInstructionsExist ||
     emptyIngredientsExist ||
@@ -68,21 +70,39 @@ export const RecipeForm = (props: RecipeFormProps) => {
       setInnerRecipe({ ...innerRecipe, [property]: value });
   };
 
+  const handleSubmit = () => {
+    if (!wasSubmitPressed) {
+      setWasSubmitPressed(true);
+    }
+
+    if (!invalidRecipe) {
+      onRecipeFormSubmit?.({ ...innerRecipe, image: innerRecipe.image! });
+    }
+  };
+
   return (
     <View style={style}>
-      <AppTextInput
-        placeholder="Recipe Title"
-        size="lg"
-        style={styles.title}
-        value={innerRecipe.title}
-        onChangeText={getInnerRecipePropertySetter("title")}
-      />
-      <ScrollView contentContainerStyle={styles.scrollView}>
-        <AppText></AppText>
-        <AppImagePicker
-          imageUri={innerRecipe.image}
-          onChangeImageUri={getInnerRecipePropertySetter("image")}
+      <View style={styles.title}>
+        <AppTextInput
+          placeholder="Recipe Title"
+          size="lg"
+          value={innerRecipe.title}
+          onChangeText={getInnerRecipePropertySetter("title")}
         />
+        {wasSubmitPressed && emptyTitle && (
+          <AppText style={styles.errorText}>Title cannot be empty</AppText>
+        )}
+      </View>
+      <ScrollView contentContainerStyle={styles.scrollView}>
+        <View>
+          <AppImagePicker
+            imageUri={innerRecipe.image}
+            onChangeImageUri={getInnerRecipePropertySetter("image")}
+          />
+          {wasSubmitPressed && noImage && (
+            <AppText style={styles.errorText}>Image must be picked</AppText>
+          )}
+        </View>
         <RecipeFormSection title="Tags">
           <AppMultiSelect
             items={categoryResponse.categories}
@@ -91,25 +111,37 @@ export const RecipeForm = (props: RecipeFormProps) => {
             selectedItems={innerRecipe.tags}
             onSelectedItemsChange={getInnerRecipePropertySetter("tags")}
           />
+          {wasSubmitPressed && noTags && (
+            <AppText style={styles.errorText}>
+              At least one tag has to be picked
+            </AppText>
+          )}
         </RecipeFormSection>
         <RecipeFormSection title="Ingredients">
           <EditableIngredients
             ingredients={innerRecipe.ingredients}
             onIngredientsChange={getInnerRecipePropertySetter("ingredients")}
           />
+          {wasSubmitPressed && emptyIngredientsExist && (
+            <AppText style={styles.errorText}>
+              Every ingredient has to have a description and a positive amount
+            </AppText>
+          )}
         </RecipeFormSection>
         <RecipeFormSection title="Instructions">
           <EditableInstructions
             instructions={innerRecipe.instructions}
             onInstructionsChange={getInnerRecipePropertySetter("instructions")}
           />
+          {wasSubmitPressed && emptyInstructionsExist && (
+            <AppText style={styles.errorText}>
+              Every instruction has to have content
+            </AppText>
+          )}
         </RecipeFormSection>
         <AppButton
-          disabled={disabledSubmitButton}
           title={recipeFormSubmitText}
-          onPress={() =>
-            onRecipeFormSubmit?.({ ...innerRecipe, image: innerRecipe.image! })
-          }
+          onPress={handleSubmit}
           size="lg"
           style={styles.uploadButton}
         />
